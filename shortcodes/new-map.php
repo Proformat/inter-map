4792299c53fc431d2ef6549ae44f045a91cc15bd
Sławomir Oruba <slawomir.oruba@gmail.com>
2023-07-13 14:22:29 +0200

    ZMIANY

--- shortcodes/new-map.php
+++ shortcodes/new-map.php
@@ -165,8 +165,10 @@ add_shortcode('new-map', function ($atts) {
 
                         $elementsData[] = array(
                             'id' => get_the_ID(),
-                            'koordynaty_1' => $koordynaty_na_budynku_tyl,
-                            'koordynaty_2' => $koordynaty_na_budynku_przod,
+                            'koordynaty' => [
+                                $koordynaty_na_budynku_przod,
+                                $koordynaty_na_budynku_tyl
+                            ],
                             'url' => $url,
                             'kolor' => $status == "Wolne" ? 'green' : 'yellow',
                             'params' => array(
@@ -181,8 +183,10 @@ add_shortcode('new-map', function ($atts) {
                 }
             }
             wp_reset_query();
-            $image_1 = rwmb_meta('przod', ['object_type' => 'term', 'size' => 'full'], get_queried_object_id()) ?? '';
-            $image_2 = rwmb_meta('tyl', ['object_type' => 'term', 'size' => 'full'], get_queried_object_id()) ?? '';
+            $backgrounds = array(
+                '1' => rwmb_meta('przod', ['object_type' => 'term', 'size' => 'full'], get_queried_object_id()) ?? '',
+                '2' => rwmb_meta('tyl', ['object_type' => 'term', 'size' => 'full'], get_queried_object_id()) ?? ''
+            );
         }
         if ($elements == 'floors') {
 
@@ -194,8 +198,7 @@ add_shortcode('new-map', function ($atts) {
         var elementsData = <?php echo json_encode($elementsData, JSON_PRETTY_PRINT); ?>;
         var currentId = <?php echo json_encode(get_the_ID()) ?>;
         var backgroundImage = <?php echo json_encode($background_image) ?>;
-        var image_1 = <?php echo json_encode($image_1) ?>;
-        var image_2 = <?php echo json_encode($image_2) ?>;
+        var backgrounds = <?php echo json_encode($backgrounds) ?>;
         var views = <?php echo json_encode($views) ?>;
     </script>
     <style>
@@ -228,168 +231,177 @@ add_shortcode('new-map', function ($atts) {
     <script>
         var sceneWidth = 1000;
         var sceneHeight = 1000;
-        var containerId = 'interactive-map';
+        var containerIdBegining = 'interactive-map-';
+        var containersSelector = '.inter-map';
 
-        let stages = {}; // create an object to hold the stages
+        const stages = {}; // create an object to hold the stages
+        const layers = {}; // create an object to hold the layers
+        const images = {}; // create an object to hold the images
         for (let index = 0; index < views; index++) {
-            stages[index] = new Konva.Stage({
-                container: containerId + '-' + (index + 1),
+            let view_id = index + 1;
+            if (view_id > 1) {
+                document.getElementById(containerIdBegining + (view_id)).style.display = "none";
+            }
+            stages[view_id] = new Konva.Stage({
+                container: containerIdBegining + view_id,
                 width: sceneWidth,
                 height: sceneHeight,
             });
-        }
-        
 
-        var stage = new Konva.Stage({
-            container: containerId,
-            width: sceneWidth,
-            height: sceneHeight,
-        });
+            layers[view_id] = new Konva.Layer();
+            stages[view_id].add(layers[view_id]);
 
-        var layer = new Konva.Layer();
-        stage.add(layer);
+            images[view_id] = new Image();
 
-        var imageObjMain = new Image();
-        imageObjMain.onload = function () {
-            var imageWidth = this.naturalWidth;
-            var imageHeight = this.naturalHeight;
+            images[view_id].onload = function () {
+                const imageWidth = this.naturalWidth;
+                const imageHeight = this.naturalHeight;
 
-            var konvaImageMain = new Konva.Image({
-                x: 0,
-                y: 0,
-                image: imageObjMain,
-                width: sceneWidth,
-                height: sceneHeight * (imageHeight / imageWidth),
-            });
+                const konvaImageMain = new Konva.Image({
+                    x: 0,
+                    y: 0,
+                    image: images[view_id],
+                    width: sceneWidth,
+                    height: sceneHeight * (imageHeight / imageWidth),
+                });
 
-            layer.add(konvaImageMain);
+                layers[view_id].add(konvaImageMain);
 
-            if (currentId == 2650) {
-                elementsData.push(
-                    {
-                        "koordynaty": "[876,559,856,731,872,731,870,748,1016,773,1019,755,1033,757,1072,583,1093,420,1097,401,1118,296,1005,282,952,377,951,387]",
-                        "url": "",
-                        "kolor": "red",
-                        "params": {
-                            "Status: ": "Inwestycja sprzedana",
-                            'Dostępnych mieszkań: ': 0
-                        }
-                    },
-                    {
-                        "koordynaty": "[867,262,854,391,768,510,737,720,725,722,724,732,605,718,605,707,594,707,589,589,628,493,633,482,624,481,664,384,682,368,682,334,721,334,738,319,739,285,783,253]",
-                        "url": "https://rafin-developer.pro-pages.com/inwestycja/wybrzeze-reymonta-ii/",
-                        "kolor": "green",
-                        "params": {
-                            "Status: ": "Dostępny",
-                            'Dostępnych mieszkań: ': 3
-                        }
-
-                    },
-                    {
-                        "koordynaty": "[1,542,71,485,67,464,229,339,386,273,384,244,393,241,392,231,460,207,515,213,517,237,526,240,529,308,460,346,402,346,403,398,371,431,331,476,147,712,1,704]",
-                        "url": "",
-                        "kolor": "black",
-                        "params": {
-                            "Status: ": "Budynek w trakcie projektowania",
-                            'Dostępnych mieszkań: ': 0
-                        }
-
-                    }
-                );
-            }
-
-            elementsData.forEach(function (element) {
-                if (element.koordynaty) {
-                    var pointsMain = element.koordynaty
-                        .replace(/[\[\]]/g, '')
-                        .split(',')
-                        .map(Number);
-
-                    // Przeskaluj punkty do nowych wymiarów obrazu
-                    for (var i = 0; i < pointsMain.length; i += 2) {
-                        pointsMain[i] = pointsMain[i] / imageWidth * sceneWidth;
-                        pointsMain[i + 1] = pointsMain[i + 1] / imageHeight * (sceneHeight * (imageHeight / imageWidth));
-                    }
-
-                    var polygonMain = new Konva.Line({
-                        points: pointsMain,
-                        fill: element.kolor,
-                        opacity: 0.3,
-                        stroke: 'black',
-                        strokeWidth: 1,
-                        closed: true
-                    });
-
-                    // Tworzenie referencji do elementów tooltip
-                    var tooltip = document.getElementById('tooltip');
-
-
-
-                    // Dodaj tooltip do zdarzenia 'mouseover'
-                    polygonMain.on('mouseover', function () {
-                        document.body.style.cursor = 'pointer';
-                        this.fill(element.kolor);
-                        this.opacity(0.5)
-                        layer.draw();
-
-                        // Append to tooltip element.params
-                        if (element.params) {
-                            // Usuń wszystkie dzieci tooltip
-                            while (tooltip.firstChild) {
-                                tooltip.removeChild(tooltip.firstChild);
+                if (currentId == 2650) {
+                    elementsData.push(
+                        {
+                            "koordynaty": "[876,559,856,731,872,731,870,748,1016,773,1019,755,1033,757,1072,583,1093,420,1097,401,1118,296,1005,282,952,377,951,387]",
+                            "url": "",
+                            "kolor": "red",
+                            "params": {
+                                "Status: ": "Inwestycja sprzedana",
+                                'Dostępnych mieszkań: ': 0
                             }
-                            for (const [key, value] of Object.entries(element.params)) {
-                                var p = document.createElement('p');
-                                p.innerText = key + value;
-                                tooltip.appendChild(p);
+                        },
+                        {
+                            "koordynaty": "[867,262,854,391,768,510,737,720,725,722,724,732,605,718,605,707,594,707,589,589,628,493,633,482,624,481,664,384,682,368,682,334,721,334,738,319,739,285,783,253]",
+                            "url": "https://rafin-developer.pro-pages.com/inwestycja/wybrzeze-reymonta-ii/",
+                            "kolor": "green",
+                            "params": {
+                                "Status: ": "Dostępny",
+                                'Dostępnych mieszkań: ': 3
                             }
+
+                        },
+                        {
+                            "koordynaty": "[1,542,71,485,67,464,229,339,386,273,384,244,393,241,392,231,460,207,515,213,517,237,526,240,529,308,460,346,402,346,403,398,371,431,331,476,147,712,1,704]",
+                            "url": "",
+                            "kolor": "black",
+                            "params": {
+                                "Status: ": "Budynek w trakcie projektowania",
+                                'Dostępnych mieszkań: ': 0
+                            }
+
                         }
-
-                        // Pokaż tooltip
-                        tooltip.style.display = 'block';
-                    });
-
-                    polygonMain.on('mouseout', function () {
-                        document.body.style.cursor = 'default';
-                        this.fill(element.kolor);
-                        this.opacity(0.3)
-                        layer.draw();
-
-                        // Ukryj tooltip
-                        tooltip.style.display = 'none';
-                    });
-
-                    // Aktualizuj pozycję tooltip na zdarzenie 'mousemove'
-                    stage.on('mousemove', function (event) {
-                        var mousePos = stage.getPointerPosition();
-                        tooltip.style.left = (mousePos.x + 50) + 'px';
-                        tooltip.style.top = (mousePos.y + 50) + 'px';
-                    });
-
-                    polygonMain.on('click', function () {
-                        window.location.href = element.url;
-                    });
-
-                    layer.add(polygonMain);
+                    );
                 }
-            });
 
-            function fitStageIntoParentContainer() {
-                var container = document.getElementById(containerId);
-                var containerWidth = container.offsetWidth;
-                var scale = containerWidth / sceneWidth;
-                console.log(containerWidth);
-                stage.width(sceneWidth * scale);
-                stage.height((sceneHeight * (imageHeight / imageWidth)) * scale);
-                stage.scale({ x: scale, y: scale });
+                elementsData.forEach(function (element) {
+                    if (element.koordynaty[view_id]) {
+                        var pointsMain = element.koordynaty[view_id]
+                            .replace(/[\[\]]/g, '')
+                            .split(',')
+                            .map(Number);
+
+                        // Przeskaluj punkty do nowych wymiarów obrazu
+                        for (var i = 0; i < pointsMain.length; i += 2) {
+                            pointsMain[i] = pointsMain[i] / imageWidth * sceneWidth;
+                            pointsMain[i + 1] = pointsMain[i + 1] / imageHeight * (sceneHeight * (imageHeight / imageWidth));
+                        }
+
+                        var polygonMain = new Konva.Line({
+                            points: pointsMain,
+                            fill: element.kolor,
+                            opacity: 0.3,
+                            stroke: 'black',
+                            strokeWidth: 1,
+                            closed: true
+                        });
+
+
+                        // Tworzenie referencji do elementów tooltip
+                        var tooltip = document.getElementById('tooltip');
+
+
+
+                        // Dodaj tooltip do zdarzenia 'mouseover'
+                        polygonMain.on('mouseover', function () {
+                            document.body.style.cursor = 'pointer';
+                            this.fill(element.kolor);
+                            this.opacity(0.5)
+                            layers[view_id].draw();
+
+                            // Append to tooltip element.params
+                            if (element.params) {
+                                // Usuń wszystkie dzieci tooltip
+                                while (tooltip.firstChild) {
+                                    tooltip.removeChild(tooltip.firstChild);
+                                }
+                                for (const [key, value] of Object.entries(element.params)) {
+                                    var p = document.createElement('p');
+                                    p.innerText = key + value;
+                                    tooltip.appendChild(p);
+                                }
+                            }
+
+                            // Pokaż tooltip
+                            tooltip.style.display = 'block';
+                        });
+
+                        polygonMain.on('mouseout', function () {
+                            document.body.style.cursor = 'default';
+                            this.fill(element.kolor);
+                            this.opacity(0.3)
+                            layers[view_id].draw();
+
+                            // Ukryj tooltip
+                            tooltip.style.display = 'none';
+                        });
+                        console.log(123);
+                        // Aktualizuj pozycję tooltip na zdarzenie 'mousemove'
+                        stages[view_id].on('mousemove', function (event) {
+                            var mousePos = stages[view_id].getPointerPosition();
+                            tooltip.style.left = (mousePos.x + 50) + 'px';
+                            tooltip.style.top = (mousePos.y + 50) + 'px';
+                        });
+
+                        polygonMain.on('click', function () {
+                            window.location.href = element.url;
+                        });
+
+                        layers[view_id].add(polygonMain);
+                    }
+                });
+
+                function fitStageIntoParentContainer() {
+                    const containers = document.querySelectorAll(containersSelector);
+                    
+                    for (let index = 0; index < containers.length; index++) {
+                        const container = containers[index];
+                        const containerWidth = container.offsetWidth;
+                        const scale = containerWidth / sceneWidth;
+                        stages[view_id].width(sceneWidth * scale);
+
+                        stages[view_id].height((sceneHeight * (imageHeight / imageWidth)) * scale);
+                        stages[view_id].scale({ x: scale, y: scale });
+                    }
+
+
+                }
+
+                fitStageIntoParentContainer();
+                window.addEventListener('resize', fitStageIntoParentContainer);
             }
-
-            fitStageIntoParentContainer();
-            window.addEventListener('resize', fitStageIntoParentContainer);
+            const imageMainUrl = backgrounds[view_id]['full_url'];
+            images[view_id].src = imageMainUrl;
         }
 
-        var imageMainUrl = backgroundImage['full_url'];
-        imageObjMain.src = imageMainUrl;
+
 
     </script>
     <?php
